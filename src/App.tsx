bbbae71b6baddf804961d8cf59ccca78e8e5707f
joyce <joycemendes.jm79@gmail.com>
2023-01-23 21:34:32 -0300

    setup inicial

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+//components
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header/>
+    
+      <h1>Conteudo...</h1>
+     
+     <Footer/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>
+            <span>To Do List</span>@2023
+        </p>
+    </footer>
+  )
+}
+
+export default Footer;
