615358410a1aa2c530bfe3d8524af8ba28416935
joyce <joycemendes.jm79@gmail.com>
2023-01-24 08:49:48 -0300

    boostrap icons

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+//CSS
+import styles from "./TaskForm.module.css";
 
 interface Props  {
     btnText:string
@@ -6,12 +7,12 @@ interface Props  {
 
 const TaskForm = ({btnText}: Props) => {
   return (
-    <form>
-        <div>
+    <form className={styles.form}>
+        <div className={styles.input_container}>
             <label htmlFor="title">Título:</label>
             <input type="text" name="title" placeholder="Título da tarefa" />
         </div>
-        <div>
+        <div className={styles.input_container}>
             <label htmlFor="difficulty">Dificuldade:</label>
             <input type="text" name="difficulty" placeholder="Dificuldade da tarefa" />
         </div>
